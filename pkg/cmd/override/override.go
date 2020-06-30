283418018617987e29d4df72e3e71c37e907e4a0
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2020-06-30 12:05:03 +0200

    Allow empty image and custom deployment

--- pkg/cmd/override/override.go
+++ pkg/cmd/override/override.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
-
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
@@ -21,10 +21,11 @@ import (
 type OverrideOptions struct {
 	configFlags *genericclioptions.ConfigFlags
 
-	args      []string
-	image     string
-	verbosity string
-	managed   bool
+	args       []string
+	image      string
+	deployment string
+	verbosity  string
+	managed    bool
 
 	dynamicClient dynamic.Interface
 	kubeClient    kubernetes.Interface
@@ -43,8 +44,9 @@ func NewOverrideOptions(streams genericclioptions.IOStreams) *OverrideOptions {
 
 var (
 	operatorOverrideExample = `
-	# override will tell cluster version operator to stop managing given operator and replace its image with custom image
-	# the 'kube-apiserver' must be valid cluster operator name (oc get clusteroperators).
+	# override will tell cluster version operator to stop managing given operator and (optionally) 
+    # replace its image with custom image the 'kube-apiserver' must be valid cluster operator name 
+    # (oc get clusteroperators).
 	%[1]s kube-apiserver --image=docker.io/foo/apiserver:debug
 
     # will make the openshift apiserver operator managed again
@@ -74,6 +76,7 @@ func NewCmdOperatorReplace(streams genericclioptions.IOStreams) *cobra.Command {
 	cmd.Flags().StringVar(&o.image, "image", o.image, "image to use for given operator")
 	cmd.Flags().StringVar(&o.verbosity, "verbosity", o.verbosity, "set the verbosity level for operator")
 	cmd.Flags().BoolVar(&o.managed, "managed", false, "set to true if you want cluster version operator to manage this operator")
+	cmd.Flags().StringVar(&o.deployment, "deployment", o.deployment, "custom deployment name")
 	o.configFlags.AddFlags(cmd.Flags())
 
 	return cmd
@@ -86,6 +89,8 @@ func getOperatorNamespace(operatorName string) string {
 	switch operatorName {
 	case "insights":
 		return "openshift-insights"
+	case "openshift-apiserver":
+		return "openshift-apiserver-operator"
 	default:
 		return "openshift-" + operatorNamespace + "-operator"
 	}
@@ -98,21 +103,21 @@ func getOperatorDeploymentName(operatorName string) string {
 }
 
 // makeJSONPatch construct the JSON patch string used to patch the clusterversion/version object
-func makeJSONPatch(operatorName string, managed bool) []byte {
+func makeJSONPatch(ns, name string, managed bool) []byte {
 	unmanagedString := "true"
 	if managed {
 		unmanagedString = "false"
 	}
 	return []byte(fmt.Sprintf(`{"spec":{"overrides":[{"group":"apps/v1","kind":"Deployment","name":"%s","namespace":"%s","unmanaged":%s}]}}`,
-		getOperatorNamespace(operatorName), getOperatorDeploymentName(operatorName), unmanagedString))
+		ns, name, unmanagedString))
 }
 
 func (o *OverrideOptions) Validate() error {
 	if len(o.args) == 0 {
 		return fmt.Errorf("clusteroperator/name must be specified")
 	}
-	if len(o.image) == 0 && !o.managed {
-		return fmt.Errorf("image must be specified")
+	if len(o.image) != 0 && o.managed {
+		return fmt.Errorf("image must be empty when operator is managed")
 	}
 	return nil
 }
@@ -155,45 +160,67 @@ func (o *OverrideOptions) Run() error {
 		return fmt.Errorf("operator %q is not valid operator: %v", o.args[0], err)
 	}
 
-	if _, err := o.dynamicClient.Resource(clusterVersionGvr).Patch("version", types.MergePatchType, makeJSONPatch(o.args[0], o.managed), metav1.PatchOptions{}); err != nil {
+	// sanity check for existence of the deployment
+	deploymentName := getOperatorDeploymentName(o.args[0])
+	if len(o.deployment) > 0 {
+		deploymentName = o.deployment
+	}
+	deploymentNS := getOperatorNamespace(o.args[0])
+	if _, err := o.kubeClient.AppsV1().Deployments(deploymentNS).Get(deploymentName, metav1.GetOptions{}); errors.IsNotFound(err) {
+		deployments, err := o.kubeClient.AppsV1().Deployments(deploymentNS).List(metav1.ListOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to get deployments in namespace %s: %v", deploymentNS, err)
+		}
+		if len(deployments.Items) == 1 {
+			deploymentName = deployments.Items[0].Name
+		} else {
+			return fmt.Errorf("deployment %s/%s not found. Maybe try --deployment for a custom name", deploymentNS, deploymentName)
+		}
+	} else if err != nil {
+		return fmt.Errorf("unable to get deployment  %s/%s: %v", deploymentNS, deploymentName, err)
+	}
+
+	if _, err := o.dynamicClient.Resource(clusterVersionGvr).Patch("version", types.MergePatchType, makeJSONPatch(deploymentNS, deploymentName, o.managed), metav1.PatchOptions{}); err != nil {
 		return fmt.Errorf("failed to patch clusterversion/version: %v", err)
 	}
 
 	// if --managed is used, patch the clusterversion to unmanaged: false and exit
 	if o.managed {
-		o.printOut("-> Operator %q now managed ...\n", getOperatorDeploymentName(o.args[0]))
+		o.printOut("-> Operator %q now managed ...\n", deploymentName)
 		return nil
 	}
 
-	o.printOut("-> Operator %q is not managed ...\n", getOperatorDeploymentName(o.args[0]))
+	o.printOut("-> Operator %q is not managed ...\n", deploymentName)
 
 	// In some case CVO will take time to reconcile new config, so give it 1s for starter
 	// TODO: The ClusterVersion operator should really reflect the current state in it's status
 	time.Sleep(1 * time.Second)
 
 	// update the operator deployment with provided image
-	// TODO: verify the operator image was really changed
-	if err := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
-		operatorDeployment, err := o.kubeClient.AppsV1().Deployments(getOperatorNamespace(o.args[0])).Get(getOperatorDeploymentName(o.args[0]), metav1.GetOptions{})
-		if err != nil {
-			return fmt.Errorf("unable to get deployment: %v", err)
-		}
-		for i := range operatorDeployment.Spec.Template.Spec.Containers {
-			operatorDeployment.Spec.Template.Spec.Containers[i].Image = o.image
-			if len(o.verbosity) > 0 {
-				operatorDeployment.Spec.Template.Spec.Containers[i].Args = append(operatorDeployment.Spec.Template.Spec.Containers[i].Args, fmt.Sprintf("-v=%s", o.verbosity))
+	if len(o.image) > 0 {
+		// TODO: verify the operator image was really changed
+		if err := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+			operatorDeployment, err := o.kubeClient.AppsV1().Deployments(deploymentNS).Get(deploymentName, metav1.GetOptions{})
+			if err != nil {
+				return fmt.Errorf("unable to get deployment: %v", err)
 			}
+			for i := range operatorDeployment.Spec.Template.Spec.Containers {
+				operatorDeployment.Spec.Template.Spec.Containers[i].Image = o.image
+				if len(o.verbosity) > 0 {
+					operatorDeployment.Spec.Template.Spec.Containers[i].Args = append(operatorDeployment.Spec.Template.Spec.Containers[i].Args, fmt.Sprintf("-v=%s", o.verbosity))
+				}
+			}
+			for i := range operatorDeployment.Spec.Template.Spec.InitContainers {
+				operatorDeployment.Spec.Template.Spec.Containers[i].Image = o.image
+			}
+			_, err = o.kubeClient.AppsV1().Deployments(deploymentNS).Update(operatorDeployment)
+			return err
+		}); err != nil {
+			return err
 		}
-		for i := range operatorDeployment.Spec.Template.Spec.InitContainers {
-			operatorDeployment.Spec.Template.Spec.Containers[i].Image = o.image
-		}
-		_, err = o.kubeClient.AppsV1().Deployments(getOperatorNamespace(o.args[0])).Update(operatorDeployment)
-		return err
-	}); err != nil {
-		return err
-	}
 
-	o.printOut("-> Operator %q image is now %q  ...\n", getOperatorDeploymentName(o.args[0]), o.image)
+		o.printOut("-> Operator %q image is now %q  ...\n", deploymentName, o.image)
+	}
 
 	return nil
 }
